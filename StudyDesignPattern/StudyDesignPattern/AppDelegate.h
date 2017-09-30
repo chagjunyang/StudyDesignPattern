c239bf4bf5f0da9a05f2238a3cfc0528e67b75c5
NHNENT <changjun.yang@nhnent.com>
2017-09-30 23:10:22 +0900

    create study design pattern project

---
+++ StudyDesignPattern/StudyDesignPattern/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StudyDesignPattern
+//
+//  Created by cjyang on 2017. 9. 30..
+//  Copyright © 2017년 NHNENT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StudyDesignPattern/StudyDesignPattern/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StudyDesignPattern
+//
+//  Created by cjyang on 2017. 9. 30..
+//  Copyright © 2017년 NHNENT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
