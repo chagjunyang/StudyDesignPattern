60ba6f780467d68d2c411f84c4491ddeec0a4706
NHNENT <changjun.yang@nhnent.com>
2017-11-03 20:51:23 +0900

    init data source

---
+++ StudyDesignPattern/StudyDesignPattern/NPPointChargeMainDataSource.m
@@ -0,0 +1,13 @@
+//
+//  NPPointChargeMainDataSource.m
+//  StudyDesignPattern
+//
+//  Created by cjyang on 2017. 11. 3..
+//  Copyright © 2017년 NHNENT. All rights reserved.
+//
+
+#import "NPPointChargeMainDataSource.h"
+
+@implementation NPPointChargeMainDataSource
+
+@end
